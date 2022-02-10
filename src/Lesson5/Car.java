e42cf3660a2d7cb957424aa1b76440db78572c26
JSilver1979 <glarosh@gmail.com>
2022-02-10 19:18:21 +0300

    HomeWork lesson 5 alternative variant commit

--- src/Lesson5/Car.java
+++ src/Lesson5/Car.java
@@ -13,6 +13,7 @@ public class Car implements Runnable {
 
     CountDownLatch prepRace;
     CountDownLatch finRace;
+    CountDownLatch startRace;
 
     public String getName() {
         return name;
@@ -20,13 +21,14 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed, CountDownLatch prepRace, CountDownLatch finRace) {
+    public Car(Race race, int speed, CountDownLatch prepRace, CountDownLatch startRace, CountDownLatch finRace) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
         this.prepRace = prepRace;
         this.finRace = finRace;
+        this.startRace = startRace;
     }
     @Override
     public void run() {
@@ -36,6 +38,7 @@ public class Car implements Runnable {
             prepRace.countDown();
             System.out.println(this.name + " готов");
             prepRace.await();
+            startRace.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/Lesson5/MainClass.java
+++ src/Lesson5/MainClass.java
@@ -8,13 +8,14 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         CountDownLatch prepRace = new CountDownLatch(CARS_COUNT);
+        CountDownLatch startRace = new CountDownLatch(1);
         CountDownLatch finRace = new CountDownLatch(CARS_COUNT);
         Semaphore tunnelCapacity = new Semaphore(CARS_COUNT/2);
         Semaphore finisher = new Semaphore(1);
-        Race race = new Race(new Road(60), new Tunnel(tunnelCapacity), new Road(100), new FinishLine(finisher));
+        Race race = new Race(new Road(60), new FinishLine(finisher));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), prepRace, finRace);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), prepRace, startRace, finRace);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
@@ -26,6 +27,7 @@ public class MainClass {
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
         try {
+            startRace.countDown();
             finRace.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
