85935a87da7908ea108e1b8a9e532041c5926504
JSilver1979 <glarosh@gmail.com>
2022-02-10 13:59:14 +0300

    HomeWork lesson 5 commit

--- src/Lesson5/Car.java
+++ src/Lesson5/Car.java
@@ -1,36 +1,53 @@
 package Lesson5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static volatile AtomicBoolean isWinner = new AtomicBoolean(false);
     static {
         CARS_COUNT = 0;
     }
     private Race race;
     private int speed;
     private String name;
+
+    CountDownLatch prepRace;
+    CountDownLatch finRace;
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch prepRace, CountDownLatch finRace) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.prepRace = prepRace;
+        this.finRace = finRace;
     }
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
+            prepRace.countDown();
             System.out.println(this.name + " готов");
+            prepRace.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        finRace.countDown();
+        if (!isWinner.getAndSet(true)) {
+            System.out.println(this.name + " Победитель!!!");
         }
     }
 }
